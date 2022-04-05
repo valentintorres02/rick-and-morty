57a42921d2be9d31be5a4c8086eb725f0ba15351
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2022-04-05 15:14:51 -0300

    initial setup: env, graphql types

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+/**
+ * Get an environment variable by name. This function will throw
+ * if the variable is not present. It will always return a string otherwise.
+ *
+ * @throws
+ * @param key string
+ * @returns string
+ */
+ export function env(key: string): string {
+  if (!key.startsWith("REACT_APP_")) {
+    throw new Error(
+      `All environment variables must be prefixed with 'REACT_APP_'. You should use 'REACT_APP_${key}' instead of '${key}'.`,
+    );
+  }
+  const value = process.env[key];
+  if (typeof value !== "string" || value.trim().length === 0) {
+    throw new Error(
+      `Missing or empty environment variable: '${key}'. Maybe you forgot to add it to your dotenv file.`,
+    );
+  }
+  return value;
+}
