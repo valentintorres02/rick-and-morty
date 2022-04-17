d9338868fd2dc5598ef993ad84feee2247c02a14
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2022-04-17 18:01:39 -0300

    refactor: Files on home container

--- src/components/Home/Home.container.tsx
+++ src/components/Home/Home.container.tsx
@@ -1,25 +1,11 @@
 import React, { Suspense, useState } from "react";
-import { Col, Row } from "antd";
 
 import CharactersList from "./Home.charactersList";
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary.container";
 import ErrorFallback from "../ErrorBoundary/ErrorBoundary.fallback";
+import HomeSkeleton from "./Home.skeleton";
 import SearchBar from "./Home.searchBar";
 import Separator from "../shared/Separator";
-import { DEFAULT_CHARACTERS_CARD } from "../../lib/utils";
-import { SkeletonCharactersCard } from "./Home.charactersCard";
-
-const HomeSkeleton: React.FC = () => {
-  return (
-    <Row gutter={[24, 24]}>
-      {Array.from({ length: DEFAULT_CHARACTERS_CARD }, (_k, v) => (
-        <Col sm={8} lg={6} xxl={4} xs={12} key={`skeleton-${v}`}>
-          <SkeletonCharactersCard />
-        </Col>
-      ))}
-    </Row>
-  );
-};
 
 const HomeContainer: React.FC = () => {
   const [nameFilter, setNameFilter] = useState<string>("");
