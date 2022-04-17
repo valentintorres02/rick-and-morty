efb3842a371627e1139c5ecbb61b7fd811627375
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2022-04-17 14:36:58 -0300

    fix: eslint warnings

--- src/routes/Character.route.tsx
+++ src/routes/Character.route.tsx
@@ -1,8 +1,6 @@
 import { useParams } from "react-router-dom";
 
-type Props = {};
-
-const CharacterRoute = (props: Props) => {
+const CharacterRoute = () => {
   const { characterId } = useParams();
 
   return <div>Character: {characterId} </div>;
